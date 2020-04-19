d2c5bfe8ae6015c9da02db15afabe20b4092f03c
leabs <stevenleabo@gmail.com>
2020-04-19 17:12:26 -0400

    lets put names here and call it a day

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Shadow Stack</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum, felis scelerisque feugiat ornare, enim sem cursus diam, quis mattis odio ipsum id ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit nunc eget libero fermentum pellentesque. Nullam vestibulum ex sed sapien efficitur semper. Morbi interdum, ipsum et feugiat convallis, magna est sagittis eros, ut feugiat ex sapien non ipsum. Integer sit amet leo at magna volutpat aliquam. Sed ac condimentum magna, eleifend euismod nulla. Proin mollis quis nisi in gravida. Praesent ut erat massa. Vestibulum malesuada ligula vitae finibus lacinia. Cras sed mauris in nisi euismod scelerisque nec sit amet risus. Suspendisse pharetra eu eros ac facilisis. Etiam dapibus nulla eget eros commodo euismod. In hac habitasse platea dictumst. </p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
